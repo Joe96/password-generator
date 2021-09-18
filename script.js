2a17f76d611f1bcfba18d15ded4c4f2e5ae3f07b
Joel Afamefune <jafamefune@gmail.com>
2021-09-18 16:20:18 -0500

    Password object can be accessed w/ generatePassword method.

--- script.js
+++ script.js
@@ -10,50 +10,83 @@ var finalPassWord = {
   hasNumbers: false,
   hasSpecialChar: false,
 
-  setPasswordLength: function () {
-
-    passwordLength = parseInt(prompt("How many character should the password have? : "));
-
-    if (this.passwordLength <= 7) {
-        console.log("Number to short, try again!")
-      } 
-      else if (this.passwordLength >= 129){
-        console.log("Number to big, try again!")
-      }
-      else {
-        return passwordLength;
-      }
-
-  },
-
-  setUppercase: function () {
-    hasUppercase = window.confirm("Should the password contain uppercase characters? ")
-    console.log("uppercase set.")
-  },
-
-  setLowercase: function () {
-    hasLowercase = window.confirm("Should the password contain lowercase characters? ")
-    console.log("lowercase set.")
-  },
-
-  setNumbers: function () {
-    hasNumbers = window.confirm("Should the password contain numbers characters? ")
-    console.log("number set.")
-  },
-
-  setSpecialChar: function () {
-    hasSpecialChar = window.confirm("Should the password contain special characters? ")
-    console.log("special character set.")
-
-  },
-
-  generatePassword: function() {
+    generatePassword: function() {
     console.log("Joel");
     finalPassWord.setPasswordLength();
     finalPassWord.setUppercase();
     finalPassWord.setLowercase();
     finalPassWord.setNumbers();
     finalPassWord.setSpecialChar();
+  },
+
+  setPasswordLength: function () {
+
+    passwordLength = parseInt(prompt("How many character should the password have? : "));
+
+    if(passwordLength <= 7) {
+      console.log("Number to short, try again!")
+    } 
+    else if (passwordLength >= 129){
+      console.log("Number to big, try again!")
+    }
+    else {
+      console.log(passwordLength)
+      return passwordLength;
+    }
+
+  },
+
+  setUppercase: function () {
+    hasUppercase = window.confirm("Should the password contain uppercase characters? ")
+
+    if(hasUppercase === true){
+      hasUppercase = true;
+      console.log(hasUppercase)
+    } else {
+      hasUppercase = false;
+      console.log(hasUppercase)
+    }
+    console.log("uppercase set.")
+  },
+
+  setLowercase: function () {
+    hasLowercase = window.confirm("Should the password contain lowercase characters? ")
+
+    if(hasLowercase === true){
+      hasLowercase = true;
+      console.log(hasLowercase)
+    } else {
+      hasLowercase = false;
+      console.log(hasLowercase)
+    }
+    console.log("lowercase set.")
+  },
+
+  setNumbers: function () {
+    hasNumbers = window.confirm("Should the password contain numbers characters? ")
+
+    if(hasNumbers === true){
+      hasNumbers = true;
+      console.log(hasNumbers)
+    } else {
+      hasNumbers = false;
+      console.log(hasNumbers)
+    }
+    console.log("number set.")
+  },
+
+  setSpecialChar: function () {
+    hasSpecialChar = window.confirm("Should the password contain special characters? ")
+
+    if(hasSpecialChar === true){
+      hasSpecialChar = true;
+      console.log(hasSpecialChar)
+    } else {
+      hasSpecialChar = false;
+      console.log(hasSpecialChar)
+    }
+    console.log("special character set.")
+
   },
 
   displayPassword: function () {
@@ -63,23 +96,14 @@ var finalPassWord = {
 }
 
 
-
-
-
-
-
-
-
-
-
 // Write password to the #password input
-  function writePassword() {
+function writePassword() {
 
-    var password = finalPassWord.generatePassword();
-    var passwordText = document.querySelector("#password");
+  var password = finalPassWord.generatePassword();
+  var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
-  }
+  passwordText.value = password;
+}
 
 
 // function generatePassword() {
